87f06075b6a02b254991e19fe227679b55927d48
zhangdaxian2017 <zhangdaxian2017@gmail.com>
2018-06-23 09:36:17 +0700

    初次上传

---
+++ Deploy.py
@@ -0,0 +1,103 @@
+import yaml
+import SFTP
+import getpass
+import hashlib
+
+
+def read_file():
+    with open('/Users/admin/Documents/deploy/Deploy2.yml', 'r') as f:
+        return yaml.load(f)
+
+
+class Param(object):
+
+    def __init__(self, values):
+        self.hosts = None
+        self.localPath = None
+        self.updateFile = None
+        self.remotePath = None
+        self.server = None
+        self.type = None
+        self.port = None
+        self.values = values
+        self.__get_host()
+        self.basic = 'basic-core-1.0-SNAPSHOT.jar'
+        self.services = 'services-1.0-SNAPSHOT.jar'
+
+    def __get_host(self):
+        self.type = self.values.get('type')
+        a = self.values.get('ip')
+        self.hosts = a.split(':')[0]
+        self.port = int(a.split(':')[1])
+        self.remotePath = self.values.get('remotePath')
+        self.updateFile = self.values.get('updateFile')
+        self.localPath = self.values.get('localPath')
+
+
+def put_files():
+    for KEY in data.keys():
+        b = Param(data.get(KEY))
+        ssh = SFTP.MySSH(b.hosts, b.port, user, passwd)
+        for keys in b.updateFile.keys():
+            if b.type == 'hz':
+                from_path = b.localPath + b.updateFile.get(keys)
+                to_path = b.remotePath + 'lib/' + b.updateFile.get(keys)
+                print("正在拷贝 %s" % b.type)
+                ssh.sftp_put(from_path, to_path)
+                cmd = "md5sum %s|cut -d ' ' -f1" % (to_path)
+                to_md5 = ssh.exe(cmd).strip()
+                from_md5 = CalcMD5(from_path)
+                # print(to_md5 == from_md5)
+                if to_md5 == from_md5:
+                    print("拷贝成功：%s " % b.updateFile.get(keys))
+                else:
+                    print("md5不正确：%s ,重新上传" % b.updateFile.get(keys))
+            else:
+                if keys == 'basic':
+                    from_path = b.localPath + b.updateFile.get(keys)
+                    to_path = b.remotePath + 'extensions/__lib__/' + b.updateFile.get(keys)
+                    print("正在拷贝 %s" % b.type)
+                    ssh.sftp_put(from_path, to_path)
+                    cmd = "md5sum %s|cut -d ' ' -f1" % (to_path)
+                    to_md5 = ssh.exe(cmd).strip()
+                    from_md5 = CalcMD5(from_path)
+                    # print(to_md5 == from_md5)
+                    if to_md5 == from_md5:
+                        print("拷贝成功：%s " % b.updateFile.get(keys))
+                    else:
+                        print("md5不正确：%s ,重新上传" % b.updateFile.get(keys))
+                else:
+                    from_path = b.localPath + b.updateFile.get(keys)
+                    to_path = b.remotePath + 'extensions/' + b.type + '/' + b.updateFile.get(keys)
+                    print("正在拷贝 %s" % b.updateFile.get(keys))
+                    ssh.sftp_put(from_path, to_path)
+                    cmd = "md5sum %s|cut -d ' ' -f1" % (to_path)
+                    to_md5 = ssh.exe(cmd).strip()
+                    from_md5 = CalcMD5(from_path)
+                    if to_md5 == from_md5:
+                        print("主机%s拷贝成功：%s " % (b.hosts, b.updateFile.get(keys)))
+                    else:
+                        print("主机%s上传文件%smd5不正确！！！！" % (b.hosts, b.updateFile.get(keys)))
+
+
+def CalcMD5(filename):
+    """
+        获取文件md5, 用来检验文件传输完整性
+    """
+    try:
+        with open(filename, 'rb') as f:
+            md5obj = hashlib.md5()
+            md5obj.update(f.read())
+            hash = md5obj.hexdigest()
+            return hash
+    except Exception as e:
+        print("本地文件不存在 %s" % e)
+
+
+if "__main__" == __name__:
+    data = read_file()
+    user = input('请输入服务器名字: ')
+    passwd = getpass.getpass()
+    print(user)
+    print(passwd)
+    put_files()
---
+++ SFTP.py
@@ -0,0 +1,71 @@
+import paramiko
+
+
+class MySSH(object):
+    """
+    1、创建一个ssh对象，将host port 用户名密码初始化，port默认值为22，用户名默认为root，密码默认空
+    2、创建ssh链接，并将目录确定到指定目录
+    3、创建sftp链接，提供调用方法，put和get
+    4、提供命令执行方法cmd
+    5、关闭链接
+    """
+
+    def __init__(self, host, port=22, username='', password=''):
+
+        """
+        :rtype: object
+        :param host: 远程主机地址
+        :param port: 远程主机端口
+        :param username: 用户名
+        :param password: 密码
+        """
+
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+
+        self.ssh = None
+        self.sftp = None
+
+        self.ssh_connect()
+        self.t = paramiko.Transport(self.host, self.port)
+
+    def ssh_connect(self):
+        try:
+            # print("开始ssh连接远程主机%s" % self.host)
+            self.ssh = paramiko.SSHClient()
+            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh.connect(self.host, self.port, username=self.username, password=self.password)
+            stdin, stdout, stderr = self.ssh.exec_command('uname -a')
+            # print(stdout.readlines())
+            # print(u'连接SSH %s 成功...' % self.host)
+        except Exception as e:
+            print('ssh %s@%s: %s' % (self.username, self.host, e))
+            # exit()
+
+    def sftp_put(self, from_path, to_path) -> object:
+        """
+        :rtype: object
+        """
+        try:
+            # print("开始sftp连接远程主机%s" % self.host)
+
+            self.t.connect(username=self.username, password=self.password)
+            self.sftp = paramiko.SFTPClient.from_transport(self.t)
+            self.sftp.put(from_path, to_path)
+            self.t.close()
+            print("%s 上传成功" % self.host)
+
+        except Exception as e:
+            print('sftp %s@%s: %s' % (self.username, self.host, e))
+
+            # exit()
+
+    def exe(self, cmd):
+        '''''
+            让远程服务器执行cmd
+        '''
+        stdin, stdout, stderr = self.ssh.exec_command(cmd)
+        if cmd.find('md5') == 0:
+            return stdout.readline()
