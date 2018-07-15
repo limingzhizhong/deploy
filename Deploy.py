6d20e7cfd28f61f7490d93631111400151567bab
zhangdaxian2017 <zhangdaxian2017@gmail.com>
2018-07-15 21:05:53 +0700

    添加日志框架

--- Deploy.py
+++ Deploy.py
@@ -4,6 +4,16 @@ import getpass
 import hashlib
 import datetime
 import sys
+import logging
+
+
+def logger():
+    logging.basicConfig(
+        filename="update.log",
+        level=logging.ERROR,
+        format="%(asctime)s %(filename)s[line:%(lineno)d]%(levelname)s - %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p"
+    )
 
 
 def read_file(file):
@@ -40,15 +50,9 @@ class Param(object):
         self.serverType = None
         self.values = values
         self.__get_host()
-        self.from_path = None
-        self.to_path = None
-        self.cmd = None
-        self.to_md5 = None
-        self.from_md5 = None
-        self.ssh = None
         self.update_hz()
         self.update_game()
-        #self.update_web()
+        self.update_web()
 
     def __get_host(self):
         self.type = self.values.get('type')
@@ -63,20 +67,20 @@ class Param(object):
         升级hz
         """
         if self.type == 'hz':
-            for self.keys in self.updateFile.keys():
+            for keys in self.updateFile.keys():
                 if self.type == 'hz':
-                    for self.index in range(len(self.hosts)):
-                        self.ssh = SFTP.MySSH(
-                            host=self.hosts[self.index].split(':')[0],
-                            port=int(self.hosts[self.index].split(':')[1]),
+                    for index in range(len(self.hosts)):
+                        ssh = SFTP.MySSH(
+                            host=self.hosts[index].split(':')[0],
+                            port=int(self.hosts[index].split(':')[1]),
                             username=self.username,
                             password=self.password
                         )
-                        for self.i in range(len(self.remotePath)):
-                            self.from_path = self.localPath + self.updateFile.get(self.keys)
-                            self.to_path = self.remotePath[self.i] + 'lib/' + self.updateFile.get(self.keys)
-                            self.ssh.sftp_put(self.from_path, self.to_path)
-                            self.__checkMD5()
+                        for i in range(len(self.remotePath)):
+                            from_path = self.localPath + self.updateFile.get(keys)
+                            to_path = self.remotePath[i] + 'lib/' + self.updateFile.get(keys)
+                            ssh.sftp_put(from_path, to_path)
+                            self.__checkMD5(ssh, from_path, to_path)
                 else:
                     continue
 
@@ -85,122 +89,72 @@ class Param(object):
         升级游戏
         """
         if self.type == 'game':
-            for self.keys in self.updateFile.keys():
-                if self.type == 'game' and self.keys != 'extensions' and self.keys != 'yml':
-                    for self.index in range(len(self.hosts)):
-                        self.ssh = SFTP.MySSH(
-                            host=self.hosts[self.index].split(':')[0],
-                            port=int(self.hosts[self.index].split(':')[1]),
+            for keys in self.updateFile.keys():
+                if self.type == 'game':
+                    for index in range(len(self.hosts)):
+                        ssh = SFTP.MySSH(
+                            host=self.hosts[index].split(':')[0],
+                            port=int(self.hosts[index].split(':')[1]),
                             username=self.username,
                             password=self.password
                         )
-                        for self.i in range(len(self.remotePath)):
-                            self.from_path = self.localPath + self.updateFile.get(self.keys)
-                            self.to_path = self.remotePath[self.i] + 'extensions/__lib__/' + \
-                                           self.updateFile.get(self.keys)
-                            self.ssh.sftp_put(self.from_path, self.to_path)
-                            self.__checkMD5()
-                else:
-                    if self.keys != 'yml':
-                        for self.i in range(len(self.remotePath)):
-                            self.from_path = self.localPath + self.updateFile.get(self.keys)
-                            self.to_path = self.remotePath[
-                                               self.i] + 'extensions/' + self.serverType + '/' + self.updateFile.get(
-                                self.keys)
-                            self.ssh.sftp_put(self.from_path, self.to_path)
-                            self.__checkMD5()
-                    else:
-                        for self.i in range(len(self.remotePath)):
-                            self.from_path = self.localPath + self.updateFile.get(self.keys)
-                            self.to_path = self.remotePath[self.i] + self.updateFile.get(self.keys)
-                            self.ssh.sftp_put(self.from_path, self.to_path)
-                            self.__checkMD5()
+                        if keys != 'extensions' and keys != 'yml':
+                            for i in range(len(self.remotePath)):
+                                from_path = self.localPath + self.updateFile.get(keys)
+                                to_path = self.remotePath[i] + 'extensions/__lib__/' + self.updateFile.get(keys)
+                                ssh.sftp_put(from_path, to_path)
+                                self.__checkMD5(ssh, from_path, to_path)
+                        else:
+                            if keys != 'yml':
+                                for i in range(len(self.remotePath)):
+                                    from_path = self.localPath + self.updateFile.get(keys)
+                                    to_path = self.remotePath[
+                                                  i] + 'extensions/' + self.serverType + '/' + self.updateFile.get(
+                                        keys)
+                                    ssh.sftp_put(from_path, to_path)
+                                    self.__checkMD5(ssh, from_path, to_path)
+                            else:
+                                for i in range(len(self.remotePath)):
+                                    from_path = self.localPath + self.updateFile.get(keys)
+                                    to_path = self.remotePath[i] + self.updateFile.get(keys)
+                                    ssh.sftp_put(from_path, to_path)
+                                    self.__checkMD5(ssh, from_path, to_path)
 
     def update_web(self):
         """
         升级web
         """
         if self.type == 'web':
-            for self.index in range(len(self.hosts)):
-                self.ssh = SFTP.MySSH(
-                    host=self.hosts[self.index].split(':')[0],
-                    port=int(self.hosts[self.index].split(':')[1]),
+            for index in range(len(self.hosts)):
+                ssh = SFTP.MySSH(
+                    host=self.hosts[index].split(':')[0],
+                    port=int(self.hosts[index].split(':')[1]),
                     username=self.username,
                     password=self.password
                 )
-                for self.keys in self.updateFile.keys():
-                    if self.keys != 'zip':
+                for keys in self.updateFile.keys():
+                    if keys != 'zip':
                         """
                         非全包更新扩展
                         """
                         continue
                     else:
-                        for self.i in range(len(self.remotePath)):
-                            self.cmd = "mv  %s %s" % (
-                                self.remotePath[self.i] + self.serverType + self.updateFile.get(self.keys),
-                                self.remotePath[self.i] + datetime.date.today().strftime('%Y-%m-%d') + '.war'
-                                )
-                            self.ssh.exe(self.cmd)
-                            self.from_path = self.localPath + self.updateFile.get(self.keys)
-                            self.to_path = self.remotePath[self.i] + self.updateFile.get(self.keys)
-                            self.ssh.sftp_put(self.from_path, self.to_path)
-                            self.__checkMD5()
-
-
-    def update_brnn(self):
-        """
-        升级游戏，百人牛牛
-        """
-        if self.type == 'hz' and self.serverType == 'baiRen':
-            for self.index in range(len(self.hosts)):
-                self.ssh = SFTP.MySSH(
-                    host=self.hosts[self.index].split(':')[0],
-                    port=int(self.hosts[self.index].split(':')[1]),
-                    username=self.username,
-                    password=self.password
-                )
-                for self.keys in self.updateFile.keys():
-                    if self.keys == 'baisc':
-                        for self.i in self.remotePath.keys():
-                            if self.i == 'master':
-                                self.to_path = self.remotePath.get(self.i) + 'lib/' + self.updateFile.get(self.keys)
-                                self.from_path = self.localPath + self.updateFile.get(self.keys)
-                                self.ssh.sftp_put(self.from_path, self.to_path)
-                                self.__checkMD5()
-                            else:
-                                if self.i == 'slave':
-                                    self.to_path = self.remotePath.get(
-                                        self.i) + 'extension/__lib__/' + self.updateFile.get(
-                                        self.keys)
-                                    self.from_path = self.localPath + self.remotePath.get(self.keys)
-                                    self.ssh.sftp_put(self.from_path, self.to_path)
-                                    self.__checkMD5()
-                    else:
-                        if self.keys == 'extensions':
-                            self.to_path = self.remotePath.get('slave') + 'lib/' + self.updateFile.get(self.keys)
-                            self.from_path = self.localPath + self.updateFile.get(self.keys)
-                            self.ssh.sftp_put(self.from_path, self.to_path)
-                            self.__checkMD5()
-                        else:
-                            if self.keys == 'yml':
-                                self.to_path = self.remotePath.get('slave') + self.updateFile.get(self.keys)
-                                self.from_path = self.localPath + self.updateFile.get(self.keys)
-                                self.ssh.sftp_put(self.from_path, self.to_path)
-                                self.__checkMD5()
-                            else:
-                                self.to_path = self.remotePath.get('slave') + self.updateFile.get(self.keys)
-                                self.from_path = self.localPath + self.updateFile.get(self.keys)
-
-    def update_worldcup(self):
-
-        if self.type == 'game' and self.serverType == 'worldcup':
-            for self.index in range(len(self.hosts)):
-                self.ssh = SFTP.MySSH(
-                    host=self.hosts[self.index].split(':')[0],
-                    port=int(self.hosts[self.index].split(':')[1]),
-                    username=self.username,
-                    password=self.password
-                )
+                        for i in range(len(self.remotePath)):
+                            cmd = "mv  %s %s" % (
+                                self.remotePath[i] + self.serverType + self.updateFile.get(keys),
+                                self.remotePath[i] + datetime.date.today().strftime('%Y-%m-%d') + '.zip'
+                            )
+                            ssh.exe(cmd)
+                            from_path = self.localPath + self.updateFile.get(keys)
+                            to_path = self.remotePath[i] + self.updateFile.get(keys)
+                            ssh.sftp_put(from_path, to_path)
+                            self.__checkMD5(ssh, from_path, to_path)
+                            cmd = "cd %s" % self.remotePath[i]
+                            ssh.exe(cmd)
+                            cmd = "zip %s" % self.updateFile.get(keys)
+                            ssh.exe(cmd)
+                            cmd = "\cp -r /root/conf/* %sWEB-INF/class/conf/*" % self.remotePath[i]
+                            ssh.exe(cmd)
 
     @staticmethod
     def __CallMD5(filename):
@@ -214,25 +168,26 @@ class Param(object):
                 hashes = md5obj.hexdigest()
                 return hashes
         except Exception as e:
-            print("本地文件%s不存在 %s" % (filename, e))
+            logging.error("本地文件%s不存在 %s" % (filename, e))
 
-    def __checkMD5(self):
+    def __checkMD5(self, ssh, from_path, to_path):
 
-        self.cmd = "md5sum %s|cut -d ' ' -f1" % self.to_path
-        self.to_md5 = self.ssh.exe(self.cmd).strip()
-        self.from_md5 = self.__CallMD5(self.from_path)
-        if self.to_md5 == self.from_md5:
-            print(
-                "update成功: %s %s %s %s" % (self.hosts[self.index].split(':')[0], self.type,
-                                           self.serverType, self.to_path))
+        cmd = "md5sum %s|cut -d ' ' -f1" % to_path
+        to_md5 = ssh.exe(cmd).strip()
+        from_md5 = self.__CallMD5(from_path)
+        if to_md5 == from_md5:
+            logging.error("update %s 路径%s 成功" % (ssh.host, to_path))
         else:
-            print("主机%s上传文件%smd5不正确！！！！" % (self.hosts[self.index], self.updateFile.get(self.keys)))
+            logging.error("update %s 路径%s 失败" % (ssh.host, to_path))
 
 
 if "__main__" == __name__:
-    loadfile = sys.argv[1]
-    data = read_file(loadfile)
-    username = input("请输入用户名:")
-    password = getpass.getpass()
-    for KEY in data.keys():
-        b = Param(data.get(KEY), user=username, passwd=password)
+    if len(sys.argv) < 2:
+        print("请跟上yml文件路径")
+    else:
+        logger()
+        data = read_file(sys.argv[1])
+        username = input("请输入用户名:")
+        password = getpass.getpass()
+        for KEY in data.keys():
+            b = Param(data.get(KEY), user=username, passwd=password)
--- SFTP.py
+++ SFTP.py
@@ -1,8 +1,15 @@
 import paramiko
 import sys
 
+import logging
 
 
+def logger():
+    logging.basicConfig(
+        filename='./update.log',
+        level=logging.ERROR,
+        format='%(levelname)s:%(asctime)s:%(message)s'
+    )
 
 
 class MySSH(object):
@@ -23,7 +30,7 @@ class MySSH(object):
         :param username: 用户名
         :param password: 密码
         """
-
+        logger()
         self.host = host
         self.port = port
         self.username = username
@@ -44,53 +51,52 @@ class MySSH(object):
             self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             self.ssh.connect(self.host, self.port, username=self.username, password=self.password)
             stdin, stdout, stderr = self.ssh.exec_command('rm -f /home/GameServer*/logs/*')
-            #print(stdout.readline())
-            #print(u'连接SSH %s 成功...' % self.host)
+            # print(stdout.readline())
+            # print(u'连接SSH %s 成功...' % self.host)
         except Exception as e:
-            print('ssh %s@%s:%s: %s' % (self.username, self.host, self.port, e))
+            logging.error('ssh %s@%s:%s: %s' % (self.username, self.host, self.port, e))
             exit()
 
-    def sftp_put(self, from_path, to_path) -> object:
+    def sftp_put(self, from_path, to_path):
 
         try:
             # print("开始sftp连接远程主机%s" % self.host)
-            self.t = paramiko.Transport((self.host, self.port))
-            self.t.connect(username=self.username, password=self.password)
-            self.sftp = paramiko.SFTPClient.from_transport(self.t)
-            self.sftp.put(from_path, to_path, callback=self.__callback(100, 100))
-            self.t.close()
-            print("%s 上传成功" % self.host)
+            t = paramiko.Transport((self.host, self.port))
+            t.connect(username=self.username, password=self.password)
+            sftp = paramiko.SFTPClient.from_transport(t)
+            sftp.put(from_path, to_path, callback=self.__callback(100, 100))
+            t.close()
+            logging.error("%s 上传成功" % host)
 
         except Exception as e:
-            print('sftp %s@%s: %s' % (self.username, self.host, e))
+            logging.error('sftp %s@%s: %s' % (self.username, self.host, e))
 
-    def sftp_get(self, from_path, to_path) -> object:
+    def sftp_get(self, from_path, to_path):
 
         try:
             # print("开始sftp连接远程主机%s" % self.host)
-            self.t = paramiko.Transport((self.host, self.port))
-            self.t.connect(username=self.username, password=self.password)
-            self.sftp = paramiko.SFTPClient.from_transport(self.t)
-            self.sftp.get(to_path, from_path)
-            self.t.close()
+            t = paramiko.Transport((self.host, self.port))
+            t.connect(username=self.username, password=self.password)
+            sftp = paramiko.SFTPClient.from_transport(t)
+            sftp.get(to_path, from_path)
+            t.close()
             # print("%s 上传成功" % self.host)
 
         except Exception as e:
-            print('sftp %s@%s: %s' % (self.username, self.host, e))
+            logging.error('sftp %s@%s: %s' % (self.username, self.host, e))
+
 
             # exit()
 
     def exe(self, cmd):
-        '''''
+        """
             让远程服务器执行cmd
-        '''
+        """
         try:
             stdin, stdout, stderr = self.ssh.exec_command(cmd)
-            self.a = stdout.readline()
-            #if cmd.find('md5') == 0:
         except Exception as e:
-            print("", e)
-        return self.a
+            logging.error("", e)
+        return stdout.readline()
 
     def close(self):
         self.ssh.close()
