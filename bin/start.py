e0b96f180e4c1b189746a0ca80ddfa578ac21b49
zhangdaxian2017 <zhangdaxian2017@gmail.com>
2018-07-31 14:53:22 +0700

    bug

--- bin/start.py
+++ bin/start.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':
             temp2 = values.get('ip')
             temp3 = values.get('remotePath')
             logging.error("将要更新的服务清单:IP地址:%s,服务类型:%s" % (temp2, temp1))
-        # username = input("请输入用户名:")
-        # password = getpass.getpass()
-        # for KEY in data.keys():
-        #     b = Deploy.Param(data.get(KEY), user=username, passwd=password)
-        # logging.error('更新完成')
+        username = input("请输入用户名:")
+        password = getpass.getpass()
+        for KEY in data.keys():
+            b = Deploy.Param(data.get(KEY), user=username, passwd=password)
+        logging.error('更新完成')
