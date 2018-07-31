d85e101f09df37b8ba72ae88f0d8357780f77420
zhangdaxian2017 <zhangdaxian2017@gmail.com>
2018-07-31 14:52:50 +0700

    调整结构

--- bin/GetHistroy.py
+++ bin/GetHistroy.py
@@ -5,10 +5,9 @@ class RedisData(object):
 
     def __init__(self):
         self.redis = RedisConn.RedisConn('127.0.0.1', 6379)
-        self.hash_map = 'deploy'
 
-    def save_data(self, hash_map_key, data):
-        self.redis.redis_conn().hset(self.hash_map, hash_map_key, data)
+    def save_data(self, hash_map, hash_map_key, data):
+        self.redis.redis_conn().hset(hash_map, hash_map_key, data)
         print("保存redis成功")
 
     def get_data(self, time):
--- bin/start.py
+++ bin/start.py
@@ -16,14 +16,20 @@ if __name__ == '__main__':
         Replace.ModifyFile(sys.argv[1], date_time)
         data = LoadFile.read_file(sys.argv[1])
         redis = GetHistroy.RedisData()
-        redis.save_data(date_time, data)
+        yml = 'web'
+        result = yml in sys.argv[1]
+        print(result)
+        if result:
+            redis.save_data(yml, date_time, data)
+        else:
+            redis.save_data('deploy', date_time, data)
         for values in data.values():
             temp1 = values.get('serverType')
             temp2 = values.get('ip')
             temp3 = values.get('remotePath')
             logging.error("将要更新的服务清单:IP地址:%s,服务类型:%s" % (temp2, temp1))
-        username = input("请输入用户名:")
-        password = getpass.getpass()
-        for KEY in data.keys():
-            b = Deploy.Param(data.get(KEY), user=username, passwd=password)
-        logging.error('更新完成')
+        # username = input("请输入用户名:")
+        # password = getpass.getpass()
+        # for KEY in data.keys():
+        #     b = Deploy.Param(data.get(KEY), user=username, passwd=password)
+        # logging.error('更新完成')
